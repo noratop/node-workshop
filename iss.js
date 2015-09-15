95b763ddd63778ed2a63a0e5f569109b9769bdf1
Nora Top <top.nora@gmail.com>
2015-09-15 19:40:31 +0000

    iss position exercise

---
+++ iss.js
@@ -0,0 +1,11 @@
+//Answers to the ISS exercise of the node workshop
+
+var request = require('request');
+
+request('http://api.open-notify.org/iss-now.json',function(error,response,body){
+    if (!error) {
+        var issData = JSON.parse(body);
+        
+        console.log("The ISS station has latitude of " + issData.iss_position.latitude.toFixed(2) + " and a longitude of " + issData.iss_position.longitude.toFixed(2));
+    } 
+});
