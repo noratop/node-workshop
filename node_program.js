1c0b5f8206c26cf511b3c78d4ab9d87e523c6ca0
Nora Top <top.nora@gmail.com>
2015-09-15 19:22:11 +0000

    add node_program.js file with answers about Hello Wolrd output to the console

---
+++ node_program.js
@@ -0,0 +1,5 @@
+//Nora Top 2015-09-15: this file contains my answers to the node-workshop exercise
+
+console.log('Hello World!');
+
+setTimeout(function(){console.log('Hello World Again!!')},10000);
