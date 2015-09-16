15f140b5777fa087dad02388321a4110eab68f07
Nora Top <top.nora@gmail.com>
2015-09-16 03:06:49 +0000

    updated iss.js to make distanceTo function being called only when both locations are determined

--- iss.js
+++ iss.js
@@ -1,5 +1,11 @@
 //Answers to the ISS exercise of the node workshop
 
+var request = require('request');
+var prompt = require('prompt');
+var userInput;
+var issLatLong = {};
+
+
 
 //necessary function for distanceTo
 Number.prototype.toRadians = function() {
@@ -27,21 +33,6 @@ var distanceTo = function(point1, point2, radius) {
 };
 
 
-var request = require('request');
-var prompt = require('prompt');
-var userInput;
-var issLatLong = {};
-
-request('http://api.open-notify.org/iss-now.json',function(error,response,body){
-    if (!error) {
-        issLatLong = JSON.parse(body);
-        issLatLong.lat = issLatLong.iss_position.latitude;
-        issLatLong.lng = issLatLong.iss_position.longitude;
-    } 
-    else console.log('An error occured.')
-});
-
-
 console.log('What is your current location ?');
 
 prompt.start();
@@ -51,10 +42,20 @@ prompt.get(['location'],function(error,result){
     request('https://maps.googleapis.com/maps/api/geocode/json?address='+userInput,function(error,response,body){
         if (!error) {
             var userGeoLocation = JSON.parse(body).results[0].geometry.location;
-            var distance = distanceTo(userGeoLocation,issLatLong);
             
-            console.log('You are located '+ Math.round(distance) + 'km from the ISS station');
+            request('http://api.open-notify.org/iss-now.json',function(error,response,body){
+                if (!error) {
+                    issLatLong = JSON.parse(body);
+                    issLatLong.lat = issLatLong.iss_position.latitude;
+                    issLatLong.lng = issLatLong.iss_position.longitude;
+                        
+                    var distance = distanceTo(userGeoLocation,issLatLong);
+                    
+                    console.log('You are located '+ Math.round(distance) + 'km from the ISS station');
+                } 
+                else console.log('An error has occured.')
+            });
         }
-        else console.log('An error occured.')
+        else console.log('An error has occured.')
     });
 });
